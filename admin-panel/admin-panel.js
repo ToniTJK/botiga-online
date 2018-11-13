39c9df3d3de3227431c22f101ee8180ebe29f0cb
toni <toninotor5@gmail.com>
2018-11-13 16:56:09 +0100

    admin-panel-base html-js

---
+++ admin-panel/admin-panel.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    $('#key').click(showKeys);
+    $('#game').click(showGames);
+    $('#user').click(showUsers);
+});
+
+function showKeys(){
+      alert("JJJ");
+}
+
+function showUsers(){
+    alert("JJJ");
+}
+
+function showGames(){
+    alert("JJJ");
+}
